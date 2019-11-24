1edf40c6b14df20191b203cb86cdda3989ac706f
Masato Hatayama <masato.hatayama@gmail.com>
2019-11-24 12:29:48 +0900

    refactoring

--- secret-handshake/lib/secret_handshake.ex
+++ secret-handshake/lib/secret_handshake.ex
@@ -4,12 +4,15 @@ defmodule SecretHandshake do
   @all_commands ["wink", "double blink", "close your eyes", "jump"]
 
   @spec commands(code :: integer) :: list(String.t())
-  def commands(code) do
-    result = @all_commands
+  def commands(code) when (code &&& 0b10000) > 0 do
+    Enum.reverse(handshake(code))
+  end
+  def commands(code), do: handshake(code)
+
+  defp handshake(code) do
+    @all_commands
     |> Enum.with_index
     |> Enum.filter(fn {_, i} -> (code &&& 1 <<< i) > 0 end)
     |> Enum.map(fn {v, _} -> v end)
-
-    if (code &&& 0b10000) > 0, do: Enum.reverse(result), else: result
   end
 end
