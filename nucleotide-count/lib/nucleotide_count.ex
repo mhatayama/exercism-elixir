65cfebbafa50a9ce340cf3bcf2c543740cab102d
Masato Hatayama <masato.hatayama@gmail.com>
2019-11-24 09:33:07 +0900

    refactoring

--- nucleotide-count/lib/nucleotide_count.ex
+++ nucleotide-count/lib/nucleotide_count.ex
@@ -3,13 +3,12 @@ defmodule NucleotideCount do
 
   @spec count(charlist(), char()) :: non_neg_integer()
   def count(strand, nucleotide) do
-    strand |> Enum.count(fn x -> x == nucleotide end)
+    strand |> Enum.count(&(&1 == nucleotide))
   end
 
   @spec histogram(charlist()) :: map()
   def histogram(strand) do
     @nucleotides
-    |> Enum.map(fn x -> {x, count(strand, x)} end)
-    |> Enum.into(%{})
+    |> Enum.into(%{}, fn x -> {x, count(strand, x)} end)
   end
 end
