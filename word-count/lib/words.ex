1bd1c87e157192140a9429a3e602be0b8f184cd9
Masato Hatayama <masato.hatayama@gmail.com>
2019-12-08 19:12:07 +0900

    submitted again

--- word-count/lib/words.ex
+++ word-count/lib/words.ex
@@ -1,12 +1,10 @@
 defmodule Words do
-  @punctuation_regex ~r/[^\w\s\_\-]/u
-  @splitter_regex ~r/[\s\_]+/u
+  @splitter_regex ~r/[\s(\!-\,)(\:-\@)(\[-\`)]+/u
 
   @spec count(String.t()) :: map
   def count(sentence) do
     String.downcase(sentence)
-    |> String.replace(@punctuation_regex, "")
-    |> String.split(@splitter_regex)
+    |> String.split(@splitter_regex, trim: true)
     |> Enum.group_by(&(&1))
     |> Enum.into(%{}, fn {k, v} -> {k, length(v)} end)
   end
